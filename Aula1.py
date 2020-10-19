1714cdd8d80f0014f465b58e2426f54170c67054
Guilherme-Sardinha <guilherme.sardinha@live.com>
2020-10-19 14:12:54 -0300

    Inicio

---
+++ Aula1.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+fonte = "https://github.com/alura-cursos/imersao-dados-2-2020/blob/master/MICRODADOS_ENEM_2019_SAMPLE_43278.csv?raw=true"
+
+dados = pd.read_csv(fonte)
+dados.head()
+
+
